1461e29c5b90d6bec22f0d90f231e816bdd31913
SANJAY <SANJAY@DESKTOP-OVOTIDF>
2021-10-25 00:19:01 +0100

    Add project files.

---
+++ ParttimeEmployeeUC3/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace ParttimeEmployeeUC3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Fulltime = 1;
+            int Parttime = 2;
+
+            int Emprateperhr = 20;
+
+            int emphrs = 0;
+            int empwage = 0;
+            Random random = new Random();
+            int empcheck = random.Next(0, 3);
+            if (empcheck == Fulltime)
+            {
+                emphrs = 8;
+                Console.WriteLine("Employee is full time");
+            }
+            else if (empcheck == Parttime)
+            {
+                emphrs = 4;
+                Console.WriteLine("Employee is part time");
+            }
+            else
+            {
+                emphrs = 0;
+                Console.WriteLine("Employee is absent");
+            }
+            empwage = emphrs * Emprateperhr;
+            Console.WriteLine("Employee Wage : " + empwage);
+
+            
+        }
+    }
+}
